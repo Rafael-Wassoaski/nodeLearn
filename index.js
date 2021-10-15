dee58c17470f031cb3e1dae5265a687eb4f9d42b
Rafael Wassoaski <meireles200@hotmail.com>
2021-10-14 21:28:51 -0300

    Commit inicial, com o projeto criado em nodejs e alguns exemplos de logs para aprendizado

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Este é um log');
+console.error('Este é um log de erro');
+console.assert(1===2, 'Esta mensagem só aparece se a condicao for falsa');
+let pessoa = {nome: 'Rafael', idade: 23}
+console.dir(pessoa);
+console.count('Sou count, fui chamado tantas vezes');
+console.count('Sou count, fui chamado tantas vezes');
+console.count('Sou count, fui chamado tantas vezes');
